8261543d33f3ae250cb7c83f287c3fe258c7e427
FernandoSullca <42738690+FernandoSullca@users.noreply.github.com>
2022-07-14 02:50:58 -0300

    Publicaciones favoritas

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPublicacionesDefault.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioPublicacionesDefault.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import ar.edu.unlam.tallerweb1.controladores.DatosPublicacion;
 import ar.edu.unlam.tallerweb1.modelo.*;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioPublicaciones;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioUsuario;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Service @Transactional
@@ -55,6 +57,18 @@ public class ServicioPublicacionesDefault implements ServicioPublicaciones {
 
         Favoritos publicacionFavorita = asignacionDeDatosDelFavorito(idPublicacion, usuarioId);
         repositorioPublicaciones.indicarFavorito(publicacionFavorita);
+    }
+
+    @Override
+    public List<Publicacion> buscarPublicacionFavoritas(Integer usuarioId) {
+        List <Favoritos> listaFavoritos=repositorioPublicaciones.BuscarFavoritosDelUsuario(usuarioId);
+        List<Publicacion> resultado = new LinkedList<Publicacion>();
+
+        for (Favoritos favoritoUni : listaFavoritos) {
+          resultado.add(favoritoUni.getPublicacion());
+        }
+
+        return resultado;
     }
 
     private Favoritos asignacionDeDatosDelFavorito(Integer idPublicacion, Integer usuarioId) {
